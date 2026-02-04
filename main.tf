6c782ad883f5dff039d81eb89e745ab226baf1f9
hkassaoui <kasshichem@gmail.com>
2026-02-04 17:03:57 -0500

    provider version update

--- main.tf
+++ main.tf
@@ -5,6 +5,15 @@ provider "azurerm" {
   skip_provider_registration = true
 }
 
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "3.26.0"
+    }
+  }
+}
+
 resource "azurerm_resource_group" "aks_rg" {
   name     = var.resource_group_name
   location = var.resource_group_location
